2edff1fb211960e165f08744dfd44eb553654083
Ankit Rai <ankitrai5096@gmail.com>
2024-05-28 23:52:24 +0530

    skills and social links are cganged

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const skillSections = document.querySelectorAll('.skill-section');
-    skillSections.forEach(section => {
-      section.scroll({
-        behavior: 'smooth'
-      });
-    });
-  });
